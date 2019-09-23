ddf41bc3d99457886cb91103ce8757bebb0fc07f
Jacob <jacob@perrysysinc.com>
2019-09-23 11:24:02 +0300

    restructure

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-from flask import flask
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+
+sql_pass = os.getenv('SQL_PASSWORD')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:{}@localhost/shas_app'.format(sql_pass)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Meseches(db.Model):
+    m_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    num_pages = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Meseches: {self.name}>'

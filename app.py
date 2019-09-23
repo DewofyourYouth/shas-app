92f49932fbfbc83587922073ef6b17a85825da76
Jacob <jacob@perrysysinc.com>
2019-09-23 12:26:15 +0300

    user and note

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    u_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+
 class Meseches(db.Model):
     m_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
@@ -29,13 +41,27 @@ class Page(db.Model):
     def __repr__(self):
         return f'<Page: m{self.book_id}, num {self.page_num}>'
 
+
+class Note(db.Model):
+    n_id = db.Column(db.Integer, primary_key=True)
+    note_title = db.Column(db.String(100), nullable=True)
+    note_text = db.Column(db.Text, nullable=False)
+
+    page_id = db.Column(db.Integer, db.ForeignKey(page.p_id), nullable=False)
+    page = db.relationship('Page', backref=db.backref('notes', lazy=True))
+
+
 def create_pages():
-    ms = Meseches.query.all()
-    print(ms)
-    for m in ms:
-        for i in range(m.num_pages):
-            page = Page(page_num = (i + 2), book_id= m.m_id)
-            db.session.add(page)
-            db.session.commit()
-            print('Page created!')
-    print('All pages created!')
+    """
+    A function for creating all pages of shas
+    """
+    if Page.query.all() == []:
+        ms = Meseches.query.all()
+        print(ms)
+        for m in ms:
+            for i in range(m.num_pages):
+                page = Page(page_num = (i + 2), book_id= m.m_id)
+                db.session.add(page)
+                db.session.commit()
+            print(f'Pages for {m.name} created!')
+        print('All pages created!')

9d0c9d914f8685004dae2a53d464858f06c8e2fb
Jacob <jacob@perrysysinc.com>
2019-09-23 12:09:14 +0300

    create pages

--- app.py
+++ app.py
@@ -17,3 +17,25 @@ class Meseches(db.Model):
 
     def __repr__(self):
         return f'<Meseches: {self.name}>'
+
+
+class Page(db.Model):
+    p_id = db.Column(db.Integer, primary_key=True)
+    page_num = db.Column(db.Integer, nullable=False)
+
+    book_id = db.Column(db.Integer, db.ForeignKey('meseches.m_id'), nullable=False)
+    meseches = db.relationship('Meseches', backref=db.backref('pages', lazy=True))
+
+    def __repr__(self):
+        return f'<Page: m{self.book_id}, num {self.page_num}>'
+
+def create_pages():
+    ms = Meseches.query.all()
+    print(ms)
+    for m in ms:
+        for i in range(m.num_pages):
+            page = Page(page_num = (i + 2), book_id= m.m_id)
+            db.session.add(page)
+            db.session.commit()
+            print('Page created!')
+    print('All pages created!')
